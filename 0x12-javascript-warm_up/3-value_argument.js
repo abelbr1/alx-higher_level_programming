c84d59c58a364857e1a66179fadb95400fa32934
abelbr1 <abelbrhanuab@gmail.com>
2022-10-03 13:46:03 +0000

    critical fix

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 if (process.argv[2] === undefined) {
-
   console.log('No argument');
-
-} else {
-
+} 
+else {
   console.log(process.argv[2]);
-
 }
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,20 +1,12 @@
 #!/usr/bin/node
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
-
   console.log('Missing size');
-
-} else {
-
+} 
+else {
   const x = Number(process.argv[2]);
-
   let i = 0;
-
   while (i < x) {
-
     console.log('X'.repeat(x));
-
     i++;
-
   }
-
 }
