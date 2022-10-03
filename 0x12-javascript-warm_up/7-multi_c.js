cc3610ccfb22442e78111c2cfe05e796719bf4ce
abelbr1 <abelbrhanuab@gmail.com>
2022-10-03 14:14:19 +0000

    at last

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+const x = Math.floor(Number(process.argv[2]));
+if (isNaN(x)) {
   console.log('Missing number of occurrences');
-} 
-else {
-  const x = Number(process.argv[2]);
-  let i = 0;
-  while (i < x) {
+} else {
+  for (let i = 0; i < x; i++) {
     console.log('C is fun');
-    i++;
   }
 }
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 function add (a, b) {
-  const c = a + b;
-  console.log(c);
+  return a + b;
 }
-add(Number(process.argv[2]), Number(process.argv[3]));
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
