053f1c832fbe10f4f4566e374eba7ed3c4bdfc49
abelbr1 <abelbrhanuab@gmail.com>
2022-10-04 09:59:27 +0000

    fix

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,9 +1,19 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
+class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
   }
   print () {
-    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
+      }
+      console.log(s);
+    }
   }
-};
+}
+module.exports = Rectangle;
--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,15 +1,28 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
+class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
   }
   print () {
-    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
+      }
+      console.log(s);
+    }
   }
   rotate () {
-    [this.width, this.height] = [this.height, this.width];
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
   }
   double () {
-    [this.width, this.height] = [this.width * 2, this.height * 2];
+    this.width *= 2;
+    this.height *= 2;
   }
-};
+}
+module.exports = Rectangle;
